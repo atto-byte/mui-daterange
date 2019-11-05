178029e10b748c0803ba631266afbd84017acc7d
William Luke <william@ordino.ai>
2019-11-05 11:11:49 +0300

    Improve Style

--- example/index.tsx
+++ example/index.tsx
@@ -2,17 +2,43 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import DateRangeTest from './DateRangeTest';
-import { createMuiTheme, withStyles } from '@material-ui/core/styles';
-import { ThemeProvider } from '@material-ui/styles';
+import { createMuiTheme, Theme } from '@material-ui/core/styles';
+import { ThemeProvider, makeStyles } from '@material-ui/styles';
+import { Card, Typography } from '@material-ui/core';
 const theme = createMuiTheme();
-
+const useStyles = makeStyles(() => ({
+  root: {
+    background: 'linear-gradient(to right, #2C5364, #203A43, #0F2027)',
+    height: '100vh',
+    width: '100vw',
+    display: 'grid',
+    gridTemplateRows: '1fr 2fr 1fr',
+  },
+  title: {
+    margin: 'auto',
+    color: 'white',
+    fontSize: '2rem',
+    fontFamily: theme.typography.fontFamily,
+  },
+  card: {
+    padding: 10,
+    margin: 'auto',
+    overflow: 'visible',
+  },
+}));
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <DateRangeTest />
-      </ThemeProvider>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <div className={classes.title}>
+          <strong>Material UI</strong> - DateRange Picker
+        </div>
+        <Card className={classes.card}>
+          <DateRangeTest />
+        </Card>
+      </div>
+    </ThemeProvider>
   );
 };
 
